2c8b12658326b3102dca2312b87c2cb90d4bdee9
lgcabbk <578902123@qq.com>
2017-04-20 22:23:56 +0800

    Optimize edit distance algorithm.

--- js/EditDistance.js
+++ js/EditDistance.js
@@ -1,3 +1,18 @@
+var width = 256;
+var height = 256;
+var editDistanceMatrix = new Array(height * width);
+
+//初始化矩阵的工作做一次就好啦，没必要每次比较都开一个新数组
+editDistanceMatrix[0] = 0;
+
+for (var index = 1; index < width; index++) {
+    editDistanceMatrix[index] = index;
+}
+
+for (var index = 1; index < height; index++) {
+    editDistanceMatrix[index * width] = index;
+}
+
 function computeEditDistance(str1, str2) {
     if(str1 == null || str2 == null) return null;
 
@@ -11,20 +26,20 @@ function computeEditDistance(str1, str2) {
         if(len1 == 0) return 0;
     }
 
-    var width = len2 + 1;
-    var height = len1 + 1;
-    var editDistanceMatrix = new Array(height * width);
+    // var width = len2 + 1;
+    // var height = len1 + 1;
+    // var editDistanceMatrix = new Array(height * width);
 
-    //初始化矩阵
-    editDistanceMatrix[0] = 0;
+    // //初始化矩阵
+    // editDistanceMatrix[0] = 0;
 
-    for (var index = 1; index <= len2; index++) {
-        editDistanceMatrix[index] = index;
-    }
+    // for (var index = 1; index <= len2; index++) {
+    //     editDistanceMatrix[index] = index;
+    // }
 
-    for (var index = 1; index <= len1; index++) {
-        editDistanceMatrix[index * width] = index;
-    }
+    // for (var index = 1; index <= len1; index++) {
+    //     editDistanceMatrix[index * width] = index;
+    // }
 
     var row = 0;
     var col = 0;
@@ -52,6 +67,16 @@ function computeSimilarity(str1, str2) {
 
     var maxLen = Math.max(str1.length, str2.length);
     if(maxLen == 0) return 1;
+
+    //暂时过滤掉两字符串长度相差50%的情况，以后考虑开放出参数设置
+    var len1 = str1.length;
+    var len2 = str2.length;
+    if(len1 > len2) {
+        var tmp = len1;
+        len1 = len2;
+        len2 = tmp;
+    }
+    if(len1 / len2  < 0.5) return 0;
 
     return 1 - computeEditDistance(str1, str2) / maxLen;
 }
