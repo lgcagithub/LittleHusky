0c94e3b6fb289066751abd934fa4cf8d61543ec1
lgcabbk <578902123@qq.com>
2017-05-17 01:40:13 +0800

    Make EditDistance be an object.

--- js/EditDistance.js
+++ js/EditDistance.js
@@ -1,19 +1,31 @@
-var width = 256;
-var height = 256;
-var editDistanceMatrix = new Array(height * width);
-
-//初始化矩阵的工作做一次就好啦，没必要每次比较都开一个新数组
-editDistanceMatrix[0] = 0;
-
-for (var index = 1; index < width; index++) {
-    editDistanceMatrix[index] = index;
+function EditDistance(nWidth, nHeight) {
+    this._reset(nWidth, nHeight);
 }
 
-for (var index = 1; index < height; index++) {
-    editDistanceMatrix[index * width] = index;
+EditDistance.prototype._reset = function(nWidth, nHeight) {
+    var w = nWidth || 128;
+    var h = nHeight || 128;
+    var m = new Array(h * w);
+
+    m[0] = 0;
+
+    for (var index = 1; index < w; index++) {
+        m[index] = index;
+    }
+
+    for (var index = 1; index < h; index++) {
+        m[index * w] = index;
+    }
+
+    this._nWidth = w;
+    this._nHeight = h;
+    this._matrix = m;
 }
 
-function computeEditDistance(str1, str2) {
+EditDistance.prototype.distance = function(szStr1, szStr2) {
+    var str1 = szStr1;
+    var str2 = szStr2;
+
     if(str1 == null || str2 == null) return null;
 
     var len1 = str1.length;
@@ -26,24 +38,18 @@ function computeEditDistance(str1, str2) {
         if(len1 == 0) return 0;
     }
 
-    // var width = len2 + 1;
-    // var height = len1 + 1;
-    // var editDistanceMatrix = new Array(height * width);
+    if(len1 + 1 > this._nHeight || len2 + 1 > this._nWidth) {
+        var h = len1 + 1 > this._nHeight ? this._nHeight << 1 : this._nHeight;
+        var w = len2 + 1 > this._nWidth ? this._nWidth << 1 : this._nWidth;
+        this._reset(w, h);
+    }
 
-    // //初始化矩阵
-    // editDistanceMatrix[0] = 0;
-
-    // for (var index = 1; index <= len2; index++) {
-    //     editDistanceMatrix[index] = index;
-    // }
-
-    // for (var index = 1; index <= len1; index++) {
-    //     editDistanceMatrix[index * width] = index;
-    // }
-
+    var matrix = this._matrix;
+    var width = this._nWidth;
     var row = 0;
     var col = 0;
     var isNotEqual = 0;
+
     for (var index1 = 0; index1 < len1; index1++) {
         row = index1 + 1;
         for (var index2 = 0; index2 < len2; index2++) {
@@ -51,18 +57,21 @@ function computeEditDistance(str1, str2) {
 
             isNotEqual = str1[index1] == str2[index2] ? 0 : 1;
 
-            editDistanceMatrix[row * width + col] = Math.min(
-                editDistanceMatrix[row * width + col - 1] + 1,
-                editDistanceMatrix[(row - 1) * width + col] + 1,
-                editDistanceMatrix[(row - 1) * width + col - 1] + isNotEqual
+            matrix[row * width + col] = Math.min(
+                matrix[row * width + col - 1] + 1,
+                matrix[(row - 1) * width + col] + 1,
+                matrix[(row - 1) * width + col - 1] + isNotEqual
             );
         }
     }
 
-    return editDistanceMatrix[len1 * width + len2];
+    return matrix[len1 * width + len2];
 }
 
-function computeSimilarity(str1, str2) {
+EditDistance.prototype.similarity = function(szStr1, szStr2) {
+    var str1 = szStr1;
+    var str2 = szStr2;
+
     if(str1 == null || str2 == null) return null;
 
     var maxLen = Math.max(str1.length, str2.length);
@@ -78,5 +87,5 @@ function computeSimilarity(str1, str2) {
     }
     if(len1 / len2  < 0.5) return 0;
 
-    return 1 - computeEditDistance(str1, str2) / maxLen;
+    return 1 - this.distance(str1, str2) / maxLen;
 }
